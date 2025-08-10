acaa6e9b94ca9a256900c91976644127c6ca326d
Pranjul Jaiswal <146024663+pranjuljais@users.noreply.github.com>
2025-08-10 20:14:52 +0530

    second

--- src/Components/Customization.jsx
+++ src/Components/Customization.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function Customization() {
   return (
-    <div>
-      <h1>Directive</h1>
+    <div className="m-10">
+      <button className="bg-blue-500 rounded p-2 text-white hover:bg-blue-800">
+        Submit
+      </button>
+      <button className="btn-primary"> Submit</button>
     </div>
   );
 }
